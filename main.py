d41f3b3621959f4f06b142fee4d9692a3be72c26
Gaspard Ferey <gaspard.ferey@inria.fr>
2018-08-01 15:54:20 +0200

    WIP

--- main.py
+++ main.py
@@ -4,52 +4,64 @@ import sys, time, os, urllib, urllib2, re, csv
 from bs4 import BeautifulSoup
 
 
+opener_headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0)',
+                  'Host' : 'www.oui.sncf',
+                  'Connection' : 'keep-alive',
+                  'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                  'Referer'   : 'http://www.voyages-sncf.com/'}
+
 def init_opener():
     proxy = urllib2.ProxyHandler({})
     #'http': 'http://127.0.0.1:8082', 'https': 'http://127.0.0.1:8082'})
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0)', 
-               'Referer'   : 'http://www.voyages-sncf.com/'}
     opener = urllib2.build_opener(proxy, urllib2.HTTPCookieProcessor())
-    opener.addheaders = headers.items()
+    opener.addheaders = opener_headers.items()
     return opener
 
 opener = init_opener()
 
-search_url = 'http://www.voyages-sncf.com/weblogic/expressbooking/_SvExpressBooking?' \
-             'bookingChoice=train&' \
-             'origin_city=%(origin_city)s&' \
-             'destination_city=%(destination_city)s&' \
-             'outward_date=%(outward_date)s&' \
-             'outward_time=%(outward_time)s&' \
-             'nbPassenger=1&' \
-             'PASSENGER_1_CARD=YOUNGS' \
-             'classe=2&' \
-             'train=Rechercher'
+base_url = 'http://www.oui.sncf/'
 
+search_url = base_url + 'vsc/train-ticket/'
 
-next_proposals_url = 'http://www.voyages-sncf.com/weblogic/proposals/nextTrains?hid=%s' \
-                     '&rfrr=PropositionAller_body_Trains%%20suivants'
-
-search_params = {'origin_city'     : 'Paris+(Toutes+gares+intramuros)',
-                 'destination_city': 'Saint-Malo',
-                 'outward_date'    : '31/07/2018',
-                 'outward_time'    : 21 }
-
-results = []
+search_params = { '_LANG': 'fr',
+                  'action:searchTravelLaunchTrain' : 'Rechercher',
+                  'COMFORT_CLASS' : '2',
+                  'DESTINATION_CITY' : 'Saint-Malo (Bretagne)',
+                  'DESTINATION_CITY_RR_CODE' : 'FRXSB',
+                  'INWARD_DATE' : '',
+                  'INWARD_TIME' : '6',
+                  'ORIGIN_CITY' : 'Paris (toutes gares intramuros)',
+                  'ORIGIN_CITY_RR_CODE' : 'FRPAR',
+                  'OUTWARD_DATE' : '01/08/2018',
+                  'OUTWARD_SCHEDULE_TYPE' : 'DEPARTURE_FROM',
+                  'OUTWARD_TIME' : '15',
+                  'PASSENGER_1' : 'ADULT',
+                  'PASSENGER_1_CARD' : 'YOUNG' }
 
 for k, v in search_params.items(): 
     search_params[k] = v and urllib2.quote(str(v), safe='') or ''
-    
 
-print search_url % search_params
+search_full_url = search_url + "?" + ("&".join([k + '=' + v for k,v in search_params.items()]))
 
-html = opener.open(search_url % search_params).read()
 
-proposals_url = re.search(r'<a href="([^"]+)" id="url_redirect_proposals"',
-                          html, re.M).group(1)
+print search_full_url
+
+response = opener.open(search_full_url)
+print response.geturl()
+print response.getcode()
+html = response.read()
 
 time.sleep(5) # behave as a browser waiting to be redirected
+
+
+results = []
+
+next_proposals_url = base_url + 'nextTrains?hid=%s'
+
 prop = opener.open(proposals_url)
+
+
+
 
 results.append(None)
 end_of_day = True
